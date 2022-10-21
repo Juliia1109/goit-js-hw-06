213f1e350b0d70d038eab093e4f198a62266b94f
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-10-22 02:50:40 +0300

    налаштування

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,9 +3,16 @@ function getRandomHexColor() {
 }
 
 
-const colorEl = document.querySelector('.widget')
-console.log(colorEl)
-const spanEl = document.querySelector('.color')
-console.log(spanEl)
-const buttonEl = document.querySelector('.change-color')
-console.log(buttonEl )
+const refs = {
+  colorEl:document.querySelector('.widget'),
+  spanEl:document.querySelector('.color'),
+  buttonEl:document.querySelector('.change-color'),
+}
+
+
+// const colorEl = document.querySelector('.widget')
+console.log(refs.colorEl)
+// const spanEl = document.querySelector('.color')
+console.log(refs.spanEl)
+// const buttonEl = document.querySelector('.change-color')
+console.log(refs.buttonEl )
